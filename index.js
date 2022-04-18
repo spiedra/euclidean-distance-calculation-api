856a32b5f39b876bcdee42dec3e76e3036666e46
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-04-18 00:19:12 -0600

    feat: add new root route

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// eslint-disable-next-line no-unused-vars
-const fs = require('fs')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-const xlsData = require('./src/routes/index.js')
+const euclideanDistanceRoutes = require('./src/routes/index.js')
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
 
 app.use(cors())
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/euclidean-distance-api', xlsData)
+app.use('/euclidean-distance-api', euclideanDistanceRoutes)
+app.get('/', (req, res, next) => res.send('Welcome to the Eucludean Distance API'))
 app.all('*', (req, res, next) =>
   res.send("You've tried reaching a route that doesn't exist")
 )
